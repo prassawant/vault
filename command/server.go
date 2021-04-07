46ee1f8ebaf9f85de04a408b6495f0cc9084cde3
Josh Black <raskchanky@gmail.com>
2021-04-07 16:49:25 -0700

    Make the hostname and raft-node-id headers configurable

--- command/server.go
+++ command/server.go
@@ -1314,6 +1314,8 @@ func (c *ServerCommand) Run(args []string) int {
 		MetricsHelper:             metricsHelper,
 		MetricSink:                metricSink,
 		SecureRandomReader:        secureRandomReader,
+		HostnameHeader:            config.HostnameHeader,
+		RaftNodeIDHeader:          config.RaftNodeIDHeader,
 	}
 	if c.flagDev {
 		coreConfig.EnableRaw = true
